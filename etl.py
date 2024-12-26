3ce7bf48fc41461fc1893586a3ea1f05df5b3819
Pandurang Gahininath Funde <122004953+pandurangfunde@users.noreply.github.com>
2024-12-27 01:03:07 +0530

    m

--- etl.py
+++ etl.py
@@ -4,7 +4,7 @@ from streamlit import *
 data = {    'Task' : ['ectract','transform','load'],
             'Status':['completed','inprogress','pending']
            }
-write('AVD1122333')
+write('AVD112233')
 
 df = DataFrame(data)
 write('Etl pipeline execution status')
